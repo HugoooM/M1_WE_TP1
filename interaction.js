4d6e0b72392e6e070477d9b48dbb0f3760cdf33f
Hugo M <hugo.montiege.cours@gmail.com>
2024-09-20 09:30:46 +0200

    TP1

--- interaction.js
+++ interaction.js
@@ -3,10 +3,45 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
+  this.x_initalPos = 0;
+  this.y_initialPos = 0;
+  this.x_finalPos = 0;
+  this.y_finalPos = 0;
+  this.pressed = false;
 
 	// Developper les 3 fonctions gérant les événements
 
+  this.mouseDown = function (evt){
+    this.pressed = true;
+
+    var pos = getMousePosition(canvas, evt);
+    this.x_initalPos = pos.x;
+    this.y_initialPos = pos.y;
+
+    console.log('Mouse down at : ' + this.x_initalPos + ", " + this.y_initialPos);
+  }.bind(DnD)
+
+  this.mouseMove = function (evt){
+    console.log('Mouse move');
+  }.bind(DnD)
+
+  this.mouseUp = function (evt){
+    if (this.pressed){
+      var pos = getMousePosition(canvas, evt);
+
+      this.x_finalPos = pos.x;
+      this.y_finalPos = pos.y;
+
+      this.pressed = false;
+      console.log('Mouse up at : ' + this.x_finalPos + ", " + this.y_finalPos);
+    }
+  }.bind(DnD)
+
 	// Associer les fonctions précédentes aux évènements du canvas.
+
+  canvas.addEventListener('mousedown', this.mouseDown, false);
+  canvas.addEventListener('mousemove', this.mouseMove, false);
+  canvas.addEventListener('mouseup', this.mouseUp, false);
 };
 
 
--- model.js
+++ model.js
@@ -1,3 +1,28 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+function Shape(){
+    this.color = null;
+    this.thickness = null;
+};
+
+function Drawing() {
+    this.drawing = new Array();
+};
+
+function Rectangle(){
+    Shape.call(this);
+    this.x = 0;
+    this.y = 0;
+    this.height = 0;
+    this.width = 0;
+};
+
+function Line(){
+    Shape.call(this);
+    this.x_start = 0;
+    this.y_start = 0;
+    this.x_final = 0;
+    this.y_final = 0;
+}
